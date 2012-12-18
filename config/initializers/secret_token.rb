93e43b9f8d6fd267a5210e792be07382b89c5c65
Jean-Philippe Boily <j@jipi.ca>
2012-12-17 20:41:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingerMountedRailsApp::Application.config.secret_token = 'ab8741c15c22d7a87703cf1432944571db15a1bce6435727f996b0b79488cf6f8b2e8b395745b9552641e2e43975af77c335dce0b2f4c61c05f908e76187c2ef'
